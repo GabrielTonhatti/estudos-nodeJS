7a68ccfe7b5bc59c2e74382420622a18bd619d56
Gabriel Tonhatti <gabrieltonhatti37@gmail.com>
2021-08-31 20:29:38 -0300

    Update files

--- app.js
+++ app.js
@@ -5,8 +5,26 @@ const bodyParser = require('body-parser');
 const admin = require('./routes/admin');
 const path = require('path');
 const mongoose = require('mongoose');
+const session = require('express-session');
+const flash = require('connect-flash');
 
 // Config
+// Sessão
+app.use(session({
+    secret: "cursoDeNode",
+    resave: true,
+    saveUninitialized: true
+}));
+
+app.use(flash());
+
+// Middleware
+app.use((req, res, next) => {
+    res.locals.success_msg = req.flash("success_msg");
+    res.locals.error_msg = req.flash('error_msg');
+    next();
+});
+
 // Body Parser
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
@@ -33,6 +51,10 @@ mongoose.connect("mongodb://localhost/blogapp")
 
 // Public
 app.use(express.static(path.join(__dirname, 'public')));
+app.use((req, res, next) => {
+    console.log("Oi eu sou um MiddleWare");
+    next();
+});
 
 // Rotas
 app.get('/', (req, res) => {

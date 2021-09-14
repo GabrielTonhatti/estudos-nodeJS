e4fe414e462fb9bdc7cbb69e63fc28b14c050d08
Gabriel Tonhatti <73304667+GabrielTonhatti@users.noreply.github.com>
2021-09-14 20:10:38 -0300

    Configurando variavel de ambiente

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 if(process.env.NODE_ENV == 'production') {
-    module.exports = {mongoURI: process.env.URL_DATABASE}
+    module.exports = { mongoURI: process.env.URL_DATABASE }
 } else {
     module.exports = {mongoURI: "mongodb://localhost/blogapp"}
 }
